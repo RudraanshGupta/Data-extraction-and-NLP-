ebf51139e4eb4492f2ee729d1aea329c02a9d3ae
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-10-29 22:11:41 +0530

    Update Data Extraction.py

--- Data Extraction.py
+++ Data Extraction.py
@@ -1,1 +1,16 @@
+import aiohttp
+import asyncio
+from bs4 import BeautifulSoup
+import pandas as pd
+import os
 
+# Loading the URLs from the input Excel file
+input_file = 'Input.xlsx'
+output_folder = 'scraped_articles'  # Folder to save titles and article text
+
+# Creating output folder if it doesn't exist
+if not os.path.exists(output_folder):
+    os.makedirs(output_folder)
+
+# Loading the input Excel file into a DataFrame
+df = pd.read_excel(input_file)
