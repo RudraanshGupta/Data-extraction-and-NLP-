58cdf53e583359b4e9563376d921efdf1ee29e07
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-11-10 21:22:28 +0530

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -1,1 +1,12 @@
+import os
+import re
+import pandas as pd
+import nltk
+from textblob import TextBlob
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize, sent_tokenize
 
+nltk.download('punkt_tab', force=True) # For sentence tokenization
+nltk.download('stopwords', force=True) # For stopwords filtering
+
+stop_words = set(stopwords.words('english'))
