a9190fb16904c129eb606da6166ea2defaecfb1e
RudraanshGupta <125785049+RudraanshGupta@users.noreply.github.com>
2024-11-12 19:53:50 +0530

    Update DataAnalysis.py

--- DataAnalysis.py
+++ DataAnalysis.py
@@ -10,3 +10,14 @@ nltk.download('punkt_tab', force=True) # For sentence tokenization
 nltk.download('stopwords', force=True) # For stopwords filtering
 
 stop_words = set(stopwords.words('english'))
+
+# Loading positive/negative word dictionaries
+positive_words = set(open(r"C:\Users\RUDRAANSH GUPTA\Downloads\20211030 Test Assignment\MasterDictionary\positive-words.txt").read().split())
+negative_words = set(open(r"C:\Users\RUDRAANSH GUPTA\Downloads\20211030 Test Assignment\MasterDictionary\negative-words.txt").read().split())
+
+# Function to clean text using stopwords
+def clean_text(text):
+    words = word_tokenize(text)
+    cleaned_words = [word for word in words if word.isalpha() and word.lower() not in stop_words]
+    return cleaned_words
+
